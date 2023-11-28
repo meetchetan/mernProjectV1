9f238323ea202895ddcbe52940cdedf5057afd07
meetchetan <chetang525@gmail.com>
2023-11-29 00:28:19 +0530

    added controllers to the project

---
+++ server/controllers/auth-controllers.js
@@ -0,0 +1,25 @@
+// "action" is the description of what you want to do, while "dispatch" is the function that carries out that action.
+
+// In an express.js application, a "controller" refers to a part of your code that is responsible for handling the application's logic. Controllers are typically used to process incoming requests, interact with models (data sources), and send responses back to clients. They help organize your application by separating concerns and following the MVC (Model-view-controller) design pattern.
+
+//  Home page logic
+
+const home = async (req, res) => {
+  try {
+    res.status(200).send("Welcome to MERN project using router.route");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//  Register page logic
+
+const register = async (req, res) => {
+  try {
+    res.status(200).send("Registration Page");
+  } catch (error) {
+    res.status(400).send({ msg: "Page not found" });
+  }
+};
+
+module.exports = { home, register };
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const authController = require("../controllers/auth-controllers");
 
 // router.get("/", (req, res) => {
 //   res.status(200).send("Welcome to MERN project using router");
 // });
 // BOTH WAY YOU CAN USE
 
-router.route("/").get((req, res) => {
-    res.status(200).send("Welcome to MERN project using router.route");
-  });
+router.route("/").get(authController.home);
 
-router.route("/register").get((req, res) => {
-    res.status(200).send("Registration Page");
-  });
-
+router.route("/register").get(authController.register);
 
 module.exports = router;
